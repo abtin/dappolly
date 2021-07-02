997133872da87039a6fc593060b62631919298ee
abtin <abtin.afshar@gmail.com>
2021-07-01 22:35:25 -0400

    work in progress

--- www/src/App.svelte
+++ www/src/App.svelte
@@ -1,9 +1,50 @@
 <script>
+	import Header from "./components/Header.svelte";
+    import Footer from "./components/Footer.svelte";
+    import Tabs from "./shared/Tabs.svelte";
+    import PollList from "./components/PollList.svelte";
+    import CreatePollForm from "./components/CreatePollForm.svelte";
+
+    // Tabs
+    let items = ["Current Polls", "Add New Poll"];
+    let activeItem = "Current Polls";
+    const tabChange = (e) => {
+        activeItem = e.detail;
+    };
+    // Poll
+    let polls = [
+        {
+            id: 1,
+            question: 'Python or Javascript?',
+            answerA: 'Python',
+            answerB: 'Javascript',
+            votesA: 9,
+            votesB: 15,
+        },
+    ];
+
+    const handleAdd = (e) => {
+        const poll = e.detail;
+        polls = [poll, ...polls];
+        console.log(polls);
+        activeItem = "Current Polls";
+    };
 </script>
 
+<Header/>
 <main>
-	<h3>hello dappolly</h3>
+	<Tabs {activeItem} {items} on:tabChange={tabChange}/>
+    {#if activeItem === "Current Polls"}
+		<PollList {polls}/>
+	{:else if activeItem === "Add New Poll"}
+		<CreatePollForm on:add={handleAdd}/>
+	{/if}
 </main>
+<Footer/>
 
 <style>
+	main {
+        max-width: 960px;
+        margin: 40px auto;
+    }
 </style>
